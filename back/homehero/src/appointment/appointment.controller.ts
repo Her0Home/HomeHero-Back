0a5613179f499e084e51e05033d8e42fd87d8522
Lautaro Rosso <Casadcomidaskairos@gmail.com>
2025-08-06 14:19:05 -0300

    martin

--- back/homehero/src/appointment/appointment.controller.ts
+++ back/homehero/src/appointment/appointment.controller.ts
@@ -27,7 +27,7 @@ export class AppointmentController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    console.log("hola mundo");
+    console.log("hola martin");
     return this.appointmentService.findOne(+id);
   }
 
