1a215d0d783859250ad4a3da27dfd04f858b6707
Lautaro Rosso <Casadcomidaskairos@gmail.com>
2025-08-06 22:38:11 -0300

    TareaEntitys

--- back/homehero/src/app.module.ts
+++ back/homehero/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppointmentModule } from './appointment/appointment.module';
+import { CategoryModule } from './src/category/category.module';
+import { CategoryModule } from './category/category.module';
+import { MembershipModule } from './membership/membership.module';
+import { SubcategoryModule } from './subcategory/subcategory.module';
+import { CategoryModule } from './category/category.module';
+import { CategoryModule } from './no-spec/category/category.module';
+import { CategoryModule } from './no-spec/category/category.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [AppointmentModule],
+  imports: [AppointmentModule, CategoryModule, SubcategoryModule, MembershipModule],
   controllers: [AppController],
   providers: [AppService],
 })
