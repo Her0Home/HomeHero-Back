a746c5ea8e560c6e8c389bc2381eac253a3c41a0
Lautaro Rosso <Casadcomidaskairos@gmail.com>
2025-08-06 23:54:17 -0300

    Revert "Buscar Categorua por ID"

--- back/homehero/src/subcategory/subcategory.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SubcategoryService } from './subcategory.service';
-import { SubcategoryController } from './subcategory.controller';
-
-@Module({
-  controllers: [SubcategoryController],
-  providers: [SubcategoryService],
-})
-export class SubcategoryModule {}
