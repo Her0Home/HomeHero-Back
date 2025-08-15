f361974bcb7a3e1f1b04393dc4f3b3ead6d9c823
Martin Samalvide <martinsamalvide3412@gmail.com>
2025-08-15 20:58:27 -0300

    arreglando

--- back/homehero/src/users/users.module.ts
+++ back/homehero/src/users/users.module.ts
@@ -3,11 +3,8 @@ import { UsersService } from './users.service';
 import { adminController, UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-<<<<<<< HEAD
 import { EmailModule } from 'src/email/email.module';
-=======
 import { requiresAuth } from 'express-openid-connect';
->>>>>>> f6104f792f2470498a88673daffb7c9f86e567cf
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), EmailModule],
