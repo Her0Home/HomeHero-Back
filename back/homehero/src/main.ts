0c84db96952bbf63128a914d2dffc65deeab99b9
hectorcoro13 <hector32219@gmail.com>
2025-08-17 23:46:00 -0500

    errores

--- back/homehero/src/main.ts
+++ back/homehero/src/main.ts
@@ -5,12 +5,29 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { auth } from 'express-openid-connect';
 import { getAuth0Config } from './config/auth0.config';
 import * as express from 'express';
-import session from 'express-session';
 import { Auth0Service } from './auth0/auth0.service';
+import cookieParser from 'cookie-parser'; // Importamos cookie-parser
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Set 'trust proxy' on the underlying Express instance
+  app.getHttpAdapter().getInstance().set('trust proxy', 1);
+  app.use(cookieParser()); // Usamos cookie-parser para leer las cookies
+
+  // --- MIDDLEWARE DE VERIFICACIÓN --- 
+  // Este middleware se ejecuta ANTES que el de Auth0 para inspeccionar la petición.
+  app.use((req, res, next) => {
+    if (req.path.includes('/auth0/callback')) {
+      console.log('--- DEBUG: INCOMING REQUEST TO CALLBACK ---');
+      console.log('Headers:', JSON.stringify(req.headers, null, 2));
+      // Gracias a cookie-parser, req.cookies estará poblado.
+      console.log('Cookies:', JSON.stringify(req.cookies, null, 2));
+      console.log('--- END DEBUG ---');
+    }
+    next(); // Pasamos la petición al siguiente middleware (el de Auth0)
+  });
 
   const swaggerDocument = new DocumentBuilder()
   .setTitle('HomeHero')
@@ -30,6 +47,7 @@ async function bootstrap() {
   
   const auth0Service = app.get(Auth0Service);
   const auth0Config = getAuth0Config(auth0Service);
+  
   
   app.use(auth(auth0Config));
   app.useGlobalPipes(new ValidationPipe({ 
