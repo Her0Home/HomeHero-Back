0218560ee97d2a7f10239346188846318d9eb76e
hectorcoro13 <148779316+hectorcoro13@users.noreply.github.com>
2025-08-20 08:36:55 -0500

    cambios2300

--- back/homehero/src/app.controller.ts
+++ back/homehero/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import type { Request, Response } from 'express';
 
