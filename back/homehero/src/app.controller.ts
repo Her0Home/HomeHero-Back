a855eca4f35daf96f626f5795ee60787da368493
hectorcoro13 <hector32219@gmail.com>
2025-08-20 15:27:35 -0500

    ge

--- back/homehero/src/app.controller.ts
+++ back/homehero/src/app.controller.ts
@@ -10,16 +10,14 @@ export class AppController {
   // getHello(): string {
   //   return this.appService.getHello();
   // }
-  @Get()
-  handleRoot(@Req() req: Request, @Res() res: Response) {
-    if (req.oidc && req.oidc.isAuthenticated()) {
-      // ¡Punto clave! Redirigir a una ruta específica en el frontend.
-      // El frontend se encargará de llamar a /auth0/profile para obtener el token.
-      res.redirect('https://home-hero-front-cc1o.vercel.app/auth/callback');
-    } else {
-      // Si no está autenticado, puede mostrar "Hello World!" o redirigir a la página de login del frontend.
-      res.send(this.appService.getHello());
-    }
+@Get()
+handleRoot(@Req() req: Request, @Res() res: Response) {
+  if (req.oidc && req.oidc.isAuthenticated()) {
+    // Si hay una sesión activa, te envío al frontend para que pida el token.
+    res.redirect('https://home-hero-front-cc1o.vercel.app/auth/callback');
+  } else {
+    res.send(this.appService.getHello());
   }
+}
 }
 
