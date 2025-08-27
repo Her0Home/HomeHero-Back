4582f037cbb19e1387fd89957f473c7482ce0da9
hectorcoro13 <148779316+hectorcoro13@users.noreply.github.com>
2025-08-27 08:37:01 -0500

    yafializa

--- back/homehero/src/comments/comments.controller.ts
+++ back/homehero/src/comments/comments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, UseGuards, Request,Query, ParseIntPipe, DefaultValuePipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards, Request,Query, ParseIntPipe, DefaultValuePipe ,ParseUUIDPipe } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { ResponseCommentDto } from './dto/update-comment.dto';
