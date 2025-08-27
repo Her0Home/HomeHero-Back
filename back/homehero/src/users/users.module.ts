d98fb4661db8cdcaa66ac22e76d82d52bb64723a
hectorcoro13 <hector32219@gmail.com>
2025-08-26 19:52:26 -0500

    cambiosAuth0

--- back/homehero/src/users/users.module.ts
+++ back/homehero/src/users/users.module.ts
@@ -4,14 +4,16 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { EmailModule } from 'src/email/email.module';
-import { requiresAuth } from 'express-openid-connect';
 import { CategoryModule } from 'src/category/category.module';
 import { AddresModule } from 'src/addres/addres.module';
+import { Comment } from 'src/comments/entities/comment.entity';
+import { Appointment } from 'src/appointment/entities/appointment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), EmailModule, CategoryModule, AddresModule],
+  imports: [TypeOrmModule.forFeature([User, Comment , Appointment]), EmailModule, CategoryModule, AddresModule],
   controllers: [UsersController],
   providers: [UsersService],
+   exports: [UsersService]
 })
 export class UsersModule  {}
 
