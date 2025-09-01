793d190c99cf2653c3a467afeaf88d97a869a448
hectorcoro13 <hector32219@gmail.com>
2025-08-31 19:39:56 -0500

    solucion

--- back/homehero/src/FilterComents/Common.module.ts
+++ back/homehero/src/FilterComents/Common.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ProfanityFilterService } from '../FilterComents/filterComents.service';
+import { ProfanityFilterService } from './filterComents.service';
 
 @Module({
   providers: [ProfanityFilterService],
